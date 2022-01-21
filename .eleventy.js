d580760c7cc3e90c3cacd6ce439e5dc611d090c5
babistsour <61907521+babistsour@users.noreply.github.com>
2022-01-22 01:44:36 +0200

    Add order field

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("sortByNum", (values) => {
+    return values.slice().sort((a, b) => a.data.order.localeCompare(b.data.order))
+  })
   eleventyConfig.addPassthroughCopy("./src/style.css")
   eleventyConfig.addPassthroughCopy("./src/main.js")
   eleventyConfig.addPassthroughCopy("./src/images")
   eleventyConfig.addPassthroughCopy("./src/favicons")
   eleventyConfig.addPassthroughCopy("./src/admin")
+
 
   return {
     dir: {
